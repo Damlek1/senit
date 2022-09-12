a5cb6c224062807d7ad7e8868ad5ea5ce342c0a3
Damlek1 <100087318+Damlek1@users.noreply.github.com>
2022-09-12 15:19:22 +0100

    login page commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Login from './pages/Login'
 import Register from './pages/Register'
 import './style.scss'
 
 function App() {
   return (
     <div>
-        <Register />
+        {/* <Register /> */}
+        <Login />
     </div>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import '../style.scss'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='formContainer'>
+      <div className='formWrapper'>
+        <span className='logo'>SenitChat</span>
+        <span className='register'>Login</span>
+        <form action="">
+          <input type="email" name="" id="" placeholder='email' required/>
+          <input type="password" name="" id="" placeholder='password' required/>
+
+          <button>Sign In</button>
+        </form>
+        <p>Don't have an account? Register</p>
+
+      </div>
+    </div>
   )
 }
 
